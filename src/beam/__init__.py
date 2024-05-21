a7995e14981cfa2419f644d38b67499a52bc5d88
Nick Petrovic <4001122+nickpetrovic@users.noreply.github.com>
2024-05-21 13:50:29 -0400

    export output class

--- src/beam/__init__.py
+++ src/beam/__init__.py
@@ -4,13 +4,13 @@ from beta9.abstractions.endpoint import Endpoint as endpoint
 from beta9.abstractions.function import Function as function
 from beta9.abstractions.image import Image
 from beta9.abstractions.map import Map
+from beta9.abstractions.output import Output
 from beta9.abstractions.queue import SimpleQueue as Queue
 from beta9.abstractions.taskqueue import TaskQueue as task_queue
 from beta9.abstractions.volume import Volume
 from beta9.type import GpuType, PythonVersion
 
 __all__ = [
-    "__version__",
     "Map",
     "Image",
     "Queue",
@@ -22,4 +22,5 @@ __all__ = [
     "env",
     "PythonVersion",
     "GpuType",
+    "Output",
 ]

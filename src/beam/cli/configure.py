47051a1d0fac4d3eba2199b0773ec996256f917a
nickpetrovic <4001122+nickpetrovic@users.noreply.github.com>
2024-10-30 17:13:01 -0400

    Feat: Improve configure command logic (#36)
    - Update beta9 package
    - Don't require a name for the context and default to "default"
    - If you don't want to overwrite the context, prompt for a different
    context name
    - If you exit from the overwrite/different name prompts, print out that
    no changes were made
    - When there's a successful change to the config, change the message to
    include the path of the config.
    
    Resolve BE-1998 BE-1996

--- src/beam/cli/configure.py
+++ src/beam/cli/configure.py
@@ -1,7 +1,7 @@
 import click
 from beta9 import terminal
 from beta9.cli.extraclick import ClickCommonGroup
-from beta9.config import ConfigContext, get_settings, load_config, save_config
+from beta9.config import DEFAULT_CONTEXT_NAME, ConfigContext, get_settings, load_config, save_config
 
 
 def validate_token(ctx: click.Context, param: click.Parameter, value: str):
@@ -41,7 +41,9 @@ def common(**_):
 @click.argument(
     "name",
     nargs=1,
-    required=True,
+    type=click.STRING,
+    required=False,
+    default=DEFAULT_CONTEXT_NAME,
 )
 def configure(token: str, name: str):
     settings = get_settings()
@@ -49,9 +51,25 @@ def configure(token: str, name: str):
     contexts = load_config(config_path)
 
     if name in contexts and contexts[name].is_valid():
-        text = f"Context '{name}' already exists. Overwrite?"
-        if terminal.prompt(text=text, default="n").lower() in ["n", "no"]:
-            return
+        while True:
+            if terminal.prompt(
+                text=f"Context '{name}' already exists. Overwrite? (y/n)", default="n"
+            ).lower() not in ["n", "no"]:
+                break
+
+            if terminal.prompt(
+                text="Would you like to provide a different context name? (y/n)", default="n"
+            ).lower() not in ["y", "yes"]:
+                terminal.warn(f"No changes made to {config_path}")
+                return
+
+            new_name = terminal.prompt(text="Enter a new context name")
+            if new_name in contexts:
+                terminal.warn("The context you entered already exists.")
+                continue
+
+            name = new_name
+            break
 
     context = ConfigContext(
         token=token, gateway_host=settings.gateway_host, gateway_port=settings.gateway_port
@@ -59,7 +77,6 @@ def configure(token: str, name: str):
 
     # Save context to config
     contexts[name] = context
-
     save_config(contexts=contexts, path=config_path)
 
-    terminal.success("Configured beam context 🎉!")
+    terminal.success(f"Added new context to {config_path}")
